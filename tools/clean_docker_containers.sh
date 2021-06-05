034bd4a7e87cc3908917f4bd229a38b019b11076
wentjiang <wentjiang@sina.com>
2021-06-05 17:28:58 +0800

    add clean docker containers shell

---
+++ tools/clean_docker_containers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker_container_ids=$(docker ps -a | awk -F '[ ]{2,}' '{print $1,$5}' |grep -v Up |tail -n +2 | awk '{print $1}')
+
+for line in $docker_container_ids
+do
+  echo "docker rm ${line}"
+  docker rm "${line}"
+done
