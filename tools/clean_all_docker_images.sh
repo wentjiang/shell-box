bba336d87bb5dd2b26a7b17b150e0cda4c731736
wentjiang <wentjiang@sina.com>
2021-07-25 16:31:14 +0800

    add clear all docker images shell

---
+++ tools/clean_all_docker_images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker_image_ids=$(docker images |awk -F '[ ]{2,}' '{print $1, $2, $3}' |awk '{print $3}')
+
+for line in $docker_image_ids
+do
+  echo "docker rmi $line"
+  docker rmi "$line"
+done
