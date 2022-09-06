12a7b92ff766bc61050b81872aaae0d7f650fd51
wentjiang <wentjiang@sina.com>
2022-09-06 14:55:49 +0800

    Update and rename stop_docker_containers.sh to stop_running_docker_containers.sh

--- tools/stop_docker_containers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker_container_ids=$(docker ps -a | awk -F '[ ]{2,}' '{print $1,$5}' |grep -v Up |tail -n +2 | awk '{print $1}')
-
-for line in $docker_container_ids
-do
-  echo "docker stop ${line}"
-  docker stop "${line}"
-done
